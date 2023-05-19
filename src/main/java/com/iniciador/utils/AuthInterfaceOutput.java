9b5f47baec7375e71e029eb2ff119b9190fafb8e
Eduardo <eduardomoura.moura@gmail.com>
2023-05-19 11:57:24 -0300

    feature(java-sdk): improve build and structures

--- src/main/java/com/iniciador/utils/AuthInterfaceOutput.java
+++ src/main/java/com/iniciador/utils/AuthInterfaceOutput.java
@@ -1,31 +1,9 @@
 package com.iniciador.utils;
+import lombok.Data;
 
+@Data
 public class AuthInterfaceOutput {
     private String accessToken;
     private String interfaceURL;
     private String paymentId;
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public String getInterfaceURL() {
-        return interfaceURL;
-    }
-
-    public void setInterfaceURL(String interfaceURL) {
-        this.interfaceURL = interfaceURL;
-    }
-
-    public String getPaymentId() {
-        return paymentId;
-    }
-
-    public void setPaymentId(String paymentId) {
-        this.paymentId = paymentId;
-    }
 }
--- src/main/java/com/iniciador/utils/AuthOutput.java
+++ src/main/java/com/iniciador/utils/AuthOutput.java
@@ -1,13 +1,9 @@
 package com.iniciador.utils;
 
+import lombok.Data;
+
+@Data
 public class AuthOutput {
     private String accessToken;
 
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
 }
